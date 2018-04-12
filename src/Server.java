ba42639417cab44f15fd59c48483b8a77035302d
hesham-akmal <hesham.akmal@gmail.com>
2018-04-12 18:01:51 +0200

    Passwords are now front-end encrypted and sent to server. Server DB contains encrypted passwords and are not decrypted there.
    Client connects to server at LoginActivity onCreate instead of when pressing btn.
    KeepLogin feature does not store password on Client side AT ALL for security reasons.
    Added loading screens.
    Optimized code.

--- src/Server.java
+++ src/Server.java
@@ -28,62 +28,86 @@ public class Server extends Thread {
         ObjectInputStream ois;
         ObjectOutputStream oos;
         Command command;
+        AuthUser a;
+
         try {
             //Creating ObjectOutputStream before ObjectInputStream IS A MUST to avoid blocking
             oos = new ObjectOutputStream(this.socket.getOutputStream());
             ois = new ObjectInputStream(this.socket.getInputStream());
-            System.out.println("Reading command from user");
-            //Read command from user
-            command = (Command) ois.readObject();
-            System.out.println("command read: " + command);
 
-            //send success ack
-            oos.writeObject(Command.success);
+            while (true) {
 
-            AuthUser a = (AuthUser) ois.readObject();
+                System.out.println("Reading command from user");
+                //Read command from user
+                command = (Command) ois.readObject();
+                System.out.println("command read: " + command);
 
-            if (command == Command.signIn) {
-                //Username does exist, compare passwords
-                if (Database.instance.getAuthUsers().containsKey(a.getUsername())) {
-                    if (Database.instance.getAuthUsers().get(a.getUsername()).getPassword().equals(a.getPassword())) {
-                        //Success. Username found and pass correct
-                        System.out.println("Success. Username found and pass correct");
-                        oos.writeObject(Command.success);
-                        //Update friend in Database //////////
-                        String timeStamp = new SimpleDateFormat("dd/MM/yyyy HH:mm a").format(Calendar.getInstance().getTime());
-                        Friend f = Database.instance.getFriends().get(a.getUsername());
-                        f.setStatus(Friend.Status.Online);
-                        f.setLastLogin(timeStamp);
-                        f.setIP(a.getIP());
-                    } else {
-                        //Fail. Username found but pass incorrect
-                        oos.writeObject(Command.fail); //
-                        System.out.println("Fail. Username found but pass incorrect");
-                    }
-                } else {
-                    //Username does not exists
-                    oos.writeObject(Command.fail);
-                    System.out.println("Username does not exists");
-                }
-            } else if (command == Command.signUp) { //sign up
-                //check existing username
-                if (Database.instance.getAuthUsers().containsKey(a.getUsername())) {
-                    oos.writeObject(Command.fail);
-                    System.out.println("existing username");
-                } else {//sign up success
-                    //Insert authUser in Database //////////
-                    Database.instance.addAuthUser(a);
-                    oos.writeObject(Command.success);
-                    System.out.println("created user");
-                    //Insert friend in Database //////////
-                    String timeStamp = new SimpleDateFormat("dd/MM/yyyy HH:mm a").format(Calendar.getInstance().getTime());
-                    Database.instance.addFriend(new Friend(a.getUsername(), Friend.Status.Online, timeStamp, a.getIP()));
+                switch (command) {
+                    case signIn:
+
+                        a = (AuthUser) ois.readObject();
+
+                        //Username does exist, compare passwords
+                        if (Database.instance.getAuthUsers().containsKey(a.getUsername())) {
+
+                            String pass1 = Database.instance.getAuthUsers().get(a.getUsername()).getPassword();
+                            String pass2 = a.getPassword();
+                            if (pass1.equals(pass2)) {
+                                //Success. Username found and pass correct
+                                System.out.println("Success. Username found and pass correct");
+                                oos.writeObject(Command.success);
+                                oos.flush();
+                                //Update friend in Database //////////
+                                String timeStamp = new SimpleDateFormat("dd/MM/yyyy HH:mm a").format(Calendar.getInstance().getTime());
+                                Friend f = Database.instance.getFriends().get(a.getUsername());
+                                f.setStatus(Friend.Status.Online);
+                                f.setLastLogin(timeStamp);
+                                f.setIP(a.getIP());
+                            } else {
+                                //Fail. Username found but pass incorrect
+                                oos.writeObject(Command.fail);
+                                oos.flush();
+                                System.out.println("Fail. Username found but pass incorrect");
+                            }
+                        } else {
+                            //Username does not exists
+                            oos.writeObject(Command.fail);
+                            oos.flush();
+                            System.out.println("Username does not exists");
+                        }
+
+                        break;
+
+                    case signUp:
+
+                        a = (AuthUser) ois.readObject();
+
+                        //check existing username
+                        if (Database.instance.getAuthUsers().containsKey(a.getUsername())) {
+                            oos.writeObject(Command.fail);
+                            oos.flush();
+                            System.out.println("existing username");
+                        } else {//sign up success
+                            //Insert authUser in Database //////////
+                            Database.instance.addAuthUser(a);
+                            oos.writeObject(Command.success);
+                            oos.flush();
+                            System.out.println("created user");
+                            //Insert friend in Database //////////
+                            String timeStamp = new SimpleDateFormat("dd/MM/yyyy HH:mm a").format(Calendar.getInstance().getTime());
+                            Database.instance.addFriend(new Friend(a.getUsername(), Friend.Status.Online, timeStamp, a.getIP()));
+                        }
+
+                        break;
                 }
             }
+        } catch (EOFException e) {
+            System.out.println("Probable user disconnection.");
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             try {
+                System.out.println("Closing thread for " + this.socket.getInetAddress().toString());
                 this.socket.close();
             } catch (IOException var12) {
                 var12.printStackTrace();
@@ -95,8 +119,6 @@ public class Server extends Thread {
         ServerSocket server = null;
 
         testPrintAll();
-        //Database.instance.addFriend(new Friend("asd", Friend.Status.Online, new SimpleDateFormat("dd/MM/yyyy HH:mm:ss a").format(Calendar.getInstance().getTime()),
-        //        "232.32.32.2"));
 
         try {
             server = new ServerSocket(PORT_NUMBER);
